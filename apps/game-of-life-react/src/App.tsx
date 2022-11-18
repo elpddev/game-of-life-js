5bf10bb4ffb6b0c8c19796e39ffa48c6241b31d0
Eyal Lapid <elpddev@pm.me>
2022-11-18 10:17:44 +0200

    Ui static components for board and cells Fixes #3

--- apps/game-of-life-react/src/App.tsx
+++ apps/game-of-life-react/src/App.tsx
@@ -1,7 +1,35 @@
+import { MantineProvider } from "@mantine/core";
+import { AppShell, Navbar, Header } from "@mantine/core";
 import { GameOfLifeBoard } from "./components/GameOfLifeBoard";
 
 function App() {
-  return <GameOfLifeBoard />;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={"100%"} p="xs">
+            {/* Navbar content */}
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            {/* Header content */}
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <GameOfLifeBoard />
+      </AppShell>
+    </MantineProvider>
+  );
 }
 
 export default App;
