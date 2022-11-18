fd532c338824afa4a9e6e915ca1b4d7c0c27018b
Eyal Lapid <17731302+elpddev@users.noreply.github.com>
2022-11-18 16:20:40 +0200

    add useInterval hook (#15)

--- apps/game-of-life-react/src/components/GameOfLifeBoard.tsx
+++ apps/game-of-life-react/src/components/GameOfLifeBoard.tsx
@@ -1,19 +1,24 @@
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Board } from "../types/Board";
 import { ActionBar } from "./ActionBar";
 import { GameBoard } from "./GameBoard";
+import {
+  useEffectWithLatest,
+  useEffectWithRefLatest,
+} from "./useEffectWithLatest";
 
 export function GameOfLifeBoard() {
-  const { board } = useGameOfLife();
+  const { board, toggle, isStarted } = useGameOfLifeBoard();
+
   return (
     <>
-      <ActionBar />
+      <ActionBar isOn={isStarted} onToggle={toggle} />
       <GameBoard board={board} />
     </>
   );
 }
 
-function useGameOfLife() {
+function useGameOfLifeBoard() {
   const board: Board = useMemo(() => {
     return {
       height: 8,
@@ -24,7 +29,71 @@ function useGameOfLife() {
     };
   }, []);
 
+  const { start, stop, toggle, isStarted } = useInterval({
+    intervalAmount: 1000,
+    workFn: () => {
+      console.log("** workFn");
+    },
+  });
+
   return {
     board,
+    isStarted,
+    toggle,
+  };
+}
+
+function useInterval({
+  intervalAmount,
+  workFn,
+}: {
+  intervalAmount: number;
+  workFn: () => void;
+}) {
+  const [restartAction, setRestartAction] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const start = useCallback(() => {
+    setRestartAction((prev) => true);
+  }, []);
+
+  const stop = useCallback(() => {
+    setRestartAction((prev) => false);
+  }, []);
+
+  const toggle = useCallback(() => {
+    setRestartAction((prev) => !prev);
+  }, []);
+
+  useEffectWithRefLatest(
+    (state) => {
+      if (!restartAction) {
+        setIsStarted(false);
+        return;
+      }
+
+      const timerId = setInterval(() => {
+        state.workFn();
+      }, state.intervalAmount);
+
+      setIsStarted(true);
+
+      return () => {
+        if (!timerId) {
+          return;
+        }
+
+        clearInterval(timerId);
+      };
+    },
+    [restartAction],
+    { workFn, intervalAmount }
+  );
+
+  return {
+    isStarted,
+    start,
+    stop,
+    toggle,
   };
 }
