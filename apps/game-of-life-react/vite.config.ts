5249a9ac3a382b3397ecf7bef621c8d8a78d99cc
Eyal Lapid <elpddev@pm.me>
2022-11-19 17:10:43 +0200

    fix base for gh pages

--- apps/game-of-life-react/vite.config.ts
+++ apps/game-of-life-react/vite.config.ts
@@ -4,4 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base is needed for github pages deployment
+  base: "/game-of-life-js/",
 });
